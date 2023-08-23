c6c7d5a18326f34200b309a5f1679d0985e8aae8
Eagle Yuan <eagleyuan21@gmail.com>
2023-08-22 22:40:04 -0400

    pull all race events for given year

--- data_scrapper.py
+++ data_scrapper.py
@@ -4,7 +4,14 @@ import fastf1.plotting
 
 fastf1.plotting.setup_mpl()
 
-session = fastf1.get_session(2023, 'Belgium', 'Q')
 
-session.load()
-print(session.weather_data)
+year = 2020
+
+races = []
+rounds = int(list(fastf1.get_event_schedule(year)[:]['RoundNumber'])[-1])
+
+i = rounds
+while i <= rounds:
+    race = fastf1.get_event(year, i).get_race()
+    race.load()
+    i += 1
